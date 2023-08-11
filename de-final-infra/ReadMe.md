7245828da223d58d104c6ac7e2a41ca25c241608
Dahye Kang <Dahye.Kang@ibm.com>
2023-08-12 04:19:49 +0900

    add mwaa module - not tested

--- de-final-infra/ReadMe.md
+++ de-final-infra/ReadMe.md
@@ -11,7 +11,16 @@
 4. 조작할 서비스 폴더에서 `terraform init`, `terraform plan` 및 `terraform apply`
 5. 서비스를 내려야 할 경우 `terraform destroy`
 
+- 각 폴더에는 terraform.tfvars가 존재 -> 보안상의 이유로 gitignore 처리
 ### VPC
-#### default
+
 - 가용영역 :"a", "c"
-- 
+- subnets
+  - private subnets: 4
+    - 2개는 일반 private, 2개는 rds instance 용
+    - 일반 private subnets들에 nat gateway 각각 할당되어 있음.
+    - 2개 rds instance는 subnet 그룹으로 묶여있음.
+  - public subnets: 2
+    - 인터넷 게이트웨이 할당됨.
+- s3, api gateway 엔드포인트 생성됨
+
---
+++ de-final-infra/mwaa/mwaa.tf
@@ -0,0 +1,63 @@
+#-----------------------------------------------------------
+# NOTE: MWAA Airflow environment takes minimum of 20 mins
+#-----------------------------------------------------------
+module "mwaa" {
+  source = "../.."
+
+  name              = "de-3-2-mwaa-env"
+  airflow_version   = "2.2.2"
+  environment_class = "mw1.medium"
+  create_s3_bucket  = false
+  source_bucket_arn = "arn:aws:s3:::de-3-2"
+  dag_s3_path       = "dags"
+
+  ## If uploading requirements.txt or plugins, you can enable these via these options
+  #plugins_s3_path      = "plugins.zip"
+  #requirements_s3_path = "requirements.txt"
+
+  logging_configuration = {
+    dag_processing_logs = {
+      enabled   = true
+      log_level = "INFO"
+    }
+
+    scheduler_logs = {
+      enabled   = true
+      log_level = "INFO"
+    }
+
+    task_logs = {
+      enabled   = true
+      log_level = "INFO"
+    }
+
+    webserver_logs = {
+      enabled   = true
+      log_level = "INFO"
+    }
+
+    worker_logs = {
+      enabled   = true
+      log_level = "INFO"
+    }
+  }
+
+  airflow_configuration_options = {
+    "core.load_default_connections" = "false"
+    "core.load_examples"            = "false"
+    "webserver.dag_default_view"    = "tree"
+    "webserver.dag_orientation"     = "TB"
+  }
+
+  min_workers        = 1
+  max_workers        = 2
+  vpc_id             = data.terraform_remote_state.vpc.outputs.vpc_id
+  private_subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
+
+  webserver_access_mode = "PUBLIC_ONLY"   # Choose the Private network option(PRIVATE_ONLY) if your Apache Airflow UI is only accessed within a corporate network, and you do not require access to public repositories for web server requirements installation
+  source_cidr           = ["10.${data.terraform_remote_state.vpc.outputs.cidr_numeral}.0.0/16"] # Add your IP address to access Airflow UI
+
+  tags = {
+    name="de-3-2-airflow"
+  }
+}
