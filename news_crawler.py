33c771f9aeb6fb0df18c904fb78da1b806a6f863
Jeon-peng <pengping@kakao.com>
2023-08-14 04:10:33 +0900

    update : update crawer.py

--- news_crawler.py
+++ news_crawler.py
@@ -3,14 +3,18 @@ import requests
 from datetime import datetime
 import time
 import multiprocessing as mp
+from multiprocessing import Manager
+import logging
 import os 
 import boto3 
 from dotenv import load_dotenv
 from pathlib import Path
-from bs4 import BeautifulSoup
 
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
+
 
 dotenv_path = Path(".env")
 load_dotenv(dotenv_path = dotenv_path)
@@ -18,10 +22,20 @@ load_dotenv(dotenv_path = dotenv_path)
 AWS_ACCESS_KEY_ID = os.getenv('aws_access_key_id')
 AWS_SECRET_ACCESS_KEY = os.getenv('aws_secret_access_key')
 AWS_DEFAULT_REGION = 'ap-northeast-2'
+BUCKET = 'de-3-2'
 
 URL = "https://news.naver.com/main/ranking/popularMemo.naver"
 SCRIPY_START_TIME = datetime.now().strftime('%Y-%m-%d_%H')
 SCRIPY_START_DAY = datetime.now().strftime('%Y-%m-%d')
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+# 로그 파일 핸들러 설정
+file_handler = logging.FileHandler(f'{SCRIPY_START_TIME}_log.txt')
+# 로그에 핸들러 추가
+logger.addHandler(file_handler)
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
 
 
 def scripy_news_title(url):
@@ -60,7 +74,7 @@ def scripy_news_title(url):
     
     print(f'len df_news_list is {len(result_df)}')
 
-    return result_df.reset_index(drop=True).sort_values('comment_count')
+    return result_df.reset_index(drop=True).sort_values('comment_count', ascending = False)
 
 def scrip_rank_news(press,url):
     header = {"user-agent": "Mozilla/5.0"}
@@ -93,88 +107,168 @@ def scrip_rank_news(press,url):
     return df
 
 
-def scripy_comment(url):
-    options = webdriver.ChromeOptions()
-    options.add_argument('--no-sandbox')
-    options.add_argument('--headless=new')
-    driver = webdriver.Chrome( options=options) # service=service,
-    driver.implicitly_wait(3)
-    driver.get(url)
-    driver.implicitly_wait(3)
+def scripy_comment(args):
+    url, processed_count, total_urls, failed_count, failed_urls = args  # 인자를 unpack
+
+
+    driver = None
     
     try :
-        while True:
-            driver.find_element(By.XPATH,'//*[@id="cbox_module"]/div[2]/div[9]/a/span/span/span[1]').click()
-            driver.implicitly_wait(2)
-    except:
-        print(f'{url} finish click more button ') 
+        list_sum = []
+        options = webdriver.ChromeOptions()
+        options.add_argument('--no-sandbox')
+        options.add_argument('--headless=new')
+        driver = webdriver.Chrome( options=options) # service=service,
+        driver.implicitly_wait(3)
+        driver.get(url)
+        driver.implicitly_wait(3)
+        
+        try :
+            while True:
+                driver.find_element(By.XPATH,'//*[@id="cbox_module"]/div[2]/div[9]/a/span/span/span[1]').click()
+                driver.implicitly_wait(2)
+        except:
+            # print(f'{url} finish click more button ') 
+            logger.info(f'{url} finish click more button ')
+        
+        # 댓글 작성자
+        nicknames = driver.find_elements(By.CSS_SELECTOR, 'span.u_cbox_nick')
+        list_nicknames = [nick.text for nick in nicknames]
+        
+        # 댓글 작성 시간
+        datetimes = driver.find_elements(By.CSS_SELECTOR, 'span.u_cbox_date')
+        list_datetimes = [datetime.text for datetime in datetimes]
+        
+        # 댓글 내용
+        comments = driver.find_elements(By.CSS_SELECTOR, 'span.u_cbox_contents')
+        list_comments = [comment.text for comment in comments]
+        
+        # 뉴스 작성 시간
+        created_news_time = driver.find_elements(By.CSS_SELECTOR,'span.media_end_head_info_datestamp_time')[0].get_attribute('data-date-time')
+        
+        # 세트로 변환
+        list_sum = [(nickname, datetime, comment, url, created_news_time, SCRIPY_START_TIME) for nickname, datetime, comment in zip(list_nicknames, list_datetimes, list_comments)]
+        processed_count.value += 1
+
+    except WebDriverException as e:
+        # print(f"Failed processing {url}. Reason: {e}")
+        logger.error(f"Failed processing {url}. Reason: {e}")
+        failed_count.value += 1
+        failed_urls.append((url, str(e)))  # URL과 함께 실패 이유를 저장
+
+    finally:
+        if driver:  # driver가 None이 아닌 경우에만 (즉, 초기화된 경우에만) quit() 호출
+            driver.quit()
+        # print(f'{url} is finish and len of list is {len(list_sum)} by {mp.current_process().name}')
+        # print(f'Processed: {processed_count.value}/{total_urls}. Failed: {failed_count.value}')
+        logger.info(f'{url} is finish and len of list is {len(list_sum)} by {mp.current_process().name}')
+        logger.info(f'Processed: {processed_count.value}/{total_urls}. Failed: {failed_count.value}')
     
-    # 댓글 작성자
-    nicknames = driver.find_elements(By.CSS_SELECTOR, 'span.u_cbox_nick')
-    list_nicknames = [nick.text for nick in nicknames]
-    
-    # 댓글 작성 시간
-    datetimes = driver.find_elements(By.CSS_SELECTOR, 'span.u_cbox_date')
-    list_datetimes = [datetime.text for datetime in datetimes]
-    
-    # 댓글 내용
-    comments = driver.find_elements(By.CSS_SELECTOR, 'span.u_cbox_contents')
-    list_comments = [comment.text for comment in comments]
-    
-    # 뉴스 작성 시간
-    created_news_time = driver.find_elements(By.CSS_SELECTOR,'span.media_end_head_info_datestamp_time')[0].get_attribute('data-date-time')
-    
-    # 세트로 변환
-    list_sum = [(nickname, datetime, comment, url, created_news_time, SCRIPY_START_TIME) for nickname, datetime, comment in zip(list_nicknames, list_datetimes, list_comments)]
-    
-    # 드라이버 종료
-    driver.quit()
-    
-    print(f'{url} is finish and len df is {len(list_sum)} by {mp.current_process().name}')
     return list_sum
 
 def run_multi_process_scripy_comment(url_list):
     start_time = time.time()
-    cpu_count = mp.cpu_count()
-    print(f'current cpu is {cpu_count}, and multiprocessing use {cpu_count}')
-    pool = mp.Pool(cpu_count)
+    cpu_count = mp.cpu_count() 
+    manager = Manager()
+    processed_count = manager.Value('i', 0)  # integer 타입의 공유 변수 초기화
+    failed_count = manager.Value('i', 0)    # 실패한 URL의 수를 기록하는 공유 변수
+    failed_urls = manager.list()             # 실패한 URL들을 저장하는 공유 리스트
+
+    # url_list의 각 URL과 공유 변수들, 리스트를 튜플로 묶어서 전달
+    args_list = [(url, processed_count, len(url_list), failed_count, failed_urls) for url in url_list]
     result = []
-    for row in pool.map(scripy_comment,url_list):
-        result += row
+    
+    # print(f'current cpu is {cpu_count}, and multiprocessing use {cpu_count}')
+    logger.info(f'current cpu is {cpu_count}, and multiprocessing use {cpu_count*2}')
+    pool = mp.Pool(int(cpu_count) * 2)
+    
+    
+    
+    for row_result in pool.map(scripy_comment, args_list):
+        result += row_result
     pool.close()
     pool.join()
     end_time = time.time()
+    # print(f'총 소요시간 : {end_time-start_time}')
+    # print(f'Total URLs: {len(url_list)}, Processed URLs: {processed_count.value}, Failed URLs: {failed_count.value}')
+    # print(f"List of Failed URLs: {[fail[0] for fail in failed_urls]}")  # 실패한 URL만 출력
+    # print(f"Reasons of Failure: {[fail[1] for fail in failed_urls]}")   # 실패 이유 출력
     
-    print(f'댓글 수집 걸린시간 : {end_time-start_time}')
-    return result
+    logger.info(f'총 소요시간 : {end_time-start_time}')
+    logger.info(f'Total URLs: {len(url_list)}, Processed URLs: {processed_count.value}, Failed URLs: {failed_count.value}')
+    logger.info(f"List of Failed URLs: {[fail[0] for fail in failed_urls]}")  # 실패한 URL만 출력
+    logger.info(f"Reasons of Failure: {[fail[1] for fail in failed_urls]}")   # 실패 이유 출력
 
-def load_to_s3(df, dir):
 
+    return result, list(failed_urls)  # 추가된 부분: 실패한 URL 리스트 반환
+
+
+def load_to_s3(data, dir, data_type='df'):
     client = boto3.client('s3',
-                    aws_access_key_id = AWS_ACCESS_KEY_ID,
-                    aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
-                    region_name = AWS_DEFAULT_REGION
-                    )
+                          aws_access_key_id=AWS_ACCESS_KEY_ID,
+                          aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                          region_name=AWS_DEFAULT_REGION
+                          )
 
-    bucket = 'de-3-2'
-    key = f'raw_data/news/{dir}/{SCRIPY_START_DAY}/{SCRIPY_START_TIME}.csv'  # 파일 확장자를 명시적으로 추가
+    base_key = f'raw_data/news/{dir}/{SCRIPY_START_DAY}/{SCRIPY_START_TIME}'
 
-    # 데이터프레임을 바로 S3에 업로드
-    csv_buffer = df.to_csv(index=False, encoding = 'utf-8-sig')
-    try:
-        response = client.put_object(Body=csv_buffer, Bucket=bucket, Key=key)
-        print("업로드 성공")
-    except Exception as e:
-        print("업로드 실패:", str(e))
+    # 파일 이름 중복 검사 및 수정
+    if data_type == 'df':
+        extension = '.csv'
+    elif data_type == 'log':
+        extension = '.txt'
+    else:
+        raise ValueError("Invalid data_type. Choose 'df' or 'log'.")
+
+    key = base_key + extension
+    counter = 1
+    while True:
+        try:
+            client.head_object(Bucket=BUCKET, Key=key)  # 오브젝트가 존재하는지 확인
+            counter += 1
+            key = base_key + f'-{counter}' + extension  # 기존 이름 뒤에 "-n" 추가
+        except:
+            break
+
+    # S3에 업로드
+    if data_type == 'df':
+        csv_buffer = data.to_csv(index=False, encoding='utf-8-sig')
+        try:
+            response = client.put_object(Body=csv_buffer, Bucket=BUCKET, Key=key)
+            logger.info(f"업로드 성공: {key}")
+        except Exception as e:
+            logger.error(f"업로드 실패: {e}")
+
+    elif data_type == 'log':
+        try:
+            with open(data, 'rb') as f:
+                client.upload_fileobj(f, BUCKET, key)
+            logger.info(f"업로드 성공: {key}")
+        except Exception as e:
+            logger.error(f"업로드 실패: {e}")
+
     
 
 def main():
     news_title = scripy_news_title(URL)
-    url_list = list(news_title.comment_link)
-    result = run_multi_process_scripy_comment(url_list)
+    url_list = list(news_title.comment_link)[-10:]
+    result, failed_urls = run_multi_process_scripy_comment(url_list)  # 실패한 URL 리스트 받아오기
+
     news_comment = pd.DataFrame(result, columns = ['nickname', 'comment_time', 'comment', 'comment_link', 'created_news_time','scrapy_time'])
+    for url, reason in failed_urls:
+        # print(f"Failed URL: {url}, Reason: {reason}")
+        logger.info(f"Failed URL: {url}, Reason: {reason}")
+    
     load_to_s3(news_title, 'news_title')
     load_to_s3(news_comment, 'news_comment')
+    time.sleep(3)
+    
+    load_to_s3(f'{SCRIPY_START_TIME}_log.txt', 'news_log', data_type='log')
+
+    
+        # 로그 파일 초기화 (옵션)
+    with open(f'{SCRIPY_START_TIME}_log.txt', 'w') as f:
+        f.truncate()
     
 if __name__ == '__main__':
     main()
---
+++ webtoon_crawler.py
@@ -0,0 +1,259 @@
+from bs4 import BeautifulSoup
+
+from datetime import datetime
+
+import time
+import pandas as pd
+import multiprocessing as mp
+from multiprocessing import Manager, Value
+import logging
+import os 
+import boto3 
+from dotenv import load_dotenv
+from pathlib import Path
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+dotenv_path = Path(".env")
+load_dotenv(dotenv_path = dotenv_path)
+
+BASE_URL = 'https://comic.naver.com/webtoon/weekday.nhn'
+MAX_PAGE_NUM = 20
+MAX_COMMENT_NUM = 1000
+AWS_ACCESS_KEY_ID = os.getenv('aws_access_key_id')
+AWS_SECRET_ACCESS_KEY = os.getenv('aws_secret_access_key')
+AWS_DEFAULT_REGION = 'ap-northeast-2'
+BUCKET = 'de-3-2'
+
+SCRIPY_START_TIME = datetime.now().strftime('%Y-%m-%d_%H')
+SCRIPY_START_DAY = datetime.now().strftime('%Y-%m-%d')
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+# 로그 파일 핸들러 설정
+file_handler = logging.FileHandler(f'{SCRIPY_START_TIME}_log.txt')
+# 로그에 핸들러 추가
+logger.addHandler(file_handler)
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+
+
+
+
+
+#driver 실행
+def get_driver_and_soup(url):
+    options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless=new')
+    driver = webdriver.Chrome( options=options) # service=service,
+    # driver.implicitly_wait(3) 
+    time.sleep(1)
+    driver.get(url)
+    driver.implicitly_wait(3)
+    driver.find_element(By.TAG_NAME,'body').send_keys(Keys.PAGE_DOWN)
+    SCROLL_PAUSE_TIME = 0.5
+
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load page
+        time.sleep(SCROLL_PAUSE_TIME)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+    html = driver.page_source                       # 현재 driver에 나타난 창의 page_source(html) 가져오기
+    soup = BeautifulSoup(html, 'html.parser')       # html 파싱(parsing)을 위해 BeautifulSoup에 넘겨주기
+    # print(f'finish __get_driver_and_soup__ ')
+    return  driver,soup
+
+def get_webtoon_titles(url):
+    #웹툰 기본 페이지에서 데이터 가져오기
+    driver,soup = get_driver_and_soup(url)
+    driver.close()
+    result = []
+    
+    days = soup.select('.WeekdayMainView__daily_all_item--DnTAH')
+    for day in days :
+        selected_day =day.select('.WeekdayMainView__heading--tHIYj')[0].get_text()
+        titles = day.select('.DailyListItem__info_area--aS5RC')
+        for title in titles :
+            webtoon_title = title.select('.ContentTitle__title--e3qXt')[0].get_text()
+            webtoon_url = f'https://comic.naver.com{title.find("a", class_ = "ContentTitle__title_area--x24vt")["href"]}'
+            result.append([selected_day,webtoon_title,webtoon_url])
+            # print(f'[{selected_day}]-[{webtoon_title}]-[{webtoon_url}]')
+            
+    df = pd.DataFrame(result, columns = ['day','title','webtoon_link'])
+    df['scrapy_time'] = SCRIPY_START_TIME
+    logger.info(f'웹툰 제목 수집 완료')
+    return df
+
+def get_webtoon_title_contents(title,url):
+    driver,soup = get_driver_and_soup(url)
+    driver.close()
+    
+    title_contents = []
+    title_index = soup.select('.EpisodeListList__item--M8zq4')
+    for index in title_index :
+        content_name = index.select(".EpisodeListList__title--lfIzU")[0].get_text()
+        content_link = f'https://comic.naver.com{index.find("a", class_ = "EpisodeListList__link--DdClU")["href"]}'
+        content_date = index.select(".date")[0].get_text()
+        # print(f'{content_name} - {content_link} - {content_date}')
+        title_contents.append([title,content_name,content_link,content_date,SCRIPY_START_TIME])
+
+    df = pd.DataFrame(title_contents, columns = ['title','content_name', 'content_link', 'content_date','scrapy_time'])
+    # print(f'finish __get_webtoon_title_contents__-{title}')
+    logger.info(f'목차 수집완료-{title}')
+    return df
+
+def get_comments(args):
+    url, title, content_name, current_title_num, total_titles, current_content_num, total_contents = args
+    driver, soup = get_driver_and_soup(url)
+    comment_num = 0
+
+    count = 0
+    try :
+        while(comment_num < MAX_COMMENT_NUM) :
+            if count == 0 :
+                driver.find_element(By.XPATH, '//*[@id="cbox_module"]/div/div[8]/a/span[1]').click()
+            else :
+                driver.find_element(By.XPATH, '//*[@id="cbox_module"]/div/div[7]/a/span/span/span[1]').click()
+            count += 1
+            comment_num = len(driver.find_elements(By.CSS_SELECTOR, 'div.u_cbox_area'))
+
+
+            # print(f'len comment = ', comment_num)
+            # print('count :', count)
+    except :
+        # print(f'{title} - {content_name} 의 댓글 수 : {comment_num}')
+        logger.info(f'{title} - {content_name} 의 댓글 수 : {comment_num}')
+
+    comment_nicknames = driver.find_elements(By.CSS_SELECTOR, 'span.u_cbox_nick')
+    list_nicknames = [nick.text for nick in comment_nicknames]
+
+    comment_ids = driver.find_elements(By.CSS_SELECTOR, 'span.u_cbox_id')
+    list_ids = [id.text for id in comment_ids]
+
+    comment_texts = driver.find_elements(By.CSS_SELECTOR, 'span.u_cbox_contents')
+    list_texts = [id.text for id in comment_texts]
+
+    comment_dates = driver.find_elements(By.CSS_SELECTOR, 'span.u_cbox_date')
+    list_dates=[]
+    for date in comment_dates:
+        list_dates.append(date.get_attribute("data-value")) 
+
+    list_sum = [(title, content_name, nickname, id, datetime, comment, SCRIPY_START_TIME) for nickname, id, datetime, comment in zip(list_nicknames, list_ids, list_dates, list_texts)]
+
+    driver.quit()
+    # print(f'{url} is finish and len of list is {len(list_sum)} by {mp.current_process().name}')
+    # df = pd.DataFrame(list_sum, columns = ['title','content_name','nickname','id','comment_date','comment','scrapy_time'])
+    # print(f"[Title {current_title_num}/{total_titles} | Content {current_content_num}/{total_contents}] Collected {comment_num} comments for {title} - {content_name}")
+    logger.info(f"[Title {current_title_num}/{total_titles} | Content {current_content_num}/{total_contents}] Collected {comment_num} comments for {title} - {content_name}")
+
+
+    return list_sum
+
+def run_multi_process_get_comments(contents_list, current_title_num, total_titles):
+    start_time = time.time()
+    
+    extended_contents_list = [(link, title, content_name, current_title_num, total_titles, idx+1, len(contents_list)) for idx, (link, title, content_name) in enumerate(contents_list)]
+    pool = mp.Pool(20)
+    result = []
+    for row in pool.map(get_comments, extended_contents_list):
+        result += row
+    pool.close()
+    pool.join()
+    end_time = time.time()
+    logger.info(f'총 소요시간 : {end_time-start_time}')
+    
+    return result
+
+def load_to_s3(data, dir, data_type='df'):
+    client = boto3.client('s3',
+                          aws_access_key_id=AWS_ACCESS_KEY_ID,
+                          aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                          region_name=AWS_DEFAULT_REGION
+                          )
+
+    base_key = f'raw_data/webtoon/{dir}/{SCRIPY_START_DAY}/{SCRIPY_START_TIME}'
+
+    # 파일 이름 중복 검사 및 수정
+    if data_type == 'df':
+        extension = '.csv'
+    elif data_type == 'log':
+        extension = '.txt'
+    else:
+        raise ValueError("Invalid data_type. Choose 'df' or 'log'.")
+
+    key = base_key + extension
+    counter = 1
+    while True:
+        try:
+            client.head_object(Bucket=BUCKET, Key=key)  # 오브젝트가 존재하는지 확인
+            counter += 1
+            key = base_key + f'-{counter}' + extension  # 기존 이름 뒤에 "-n" 추가
+        except:
+            break
+
+    # S3에 업로드
+    if data_type == 'df':
+        csv_buffer = data.to_csv(index=False, encoding='utf-8-sig')
+        try:
+            response = client.put_object(Body=csv_buffer, Bucket=BUCKET, Key=key)
+            logger.info(f"업로드 성공: {key}")
+        except Exception as e:
+            logger.error(f"업로드 실패: {e}")
+
+    elif data_type == 'log':
+        try:
+            with open(data, 'rb') as f:
+                client.upload_fileobj(f, BUCKET, key)
+            logger.info(f"업로드 성공: {key}")
+        except Exception as e:
+            logger.error(f"업로드 실패: {e}")
+
+
+def main():
+    # 현재 요일을 구합니다.
+    days = ["월요웹툰", "화요웹툰", "수요웹툰", "목요웹툰", "금요웹툰", "토요웹툰", "일요웹툰"]
+    current_day = days[datetime.today().weekday()]  # 0: 월요일, 1: 화요일, ...
+
+    webtoon_titles = get_webtoon_titles(BASE_URL)
+    filtered_titles = webtoon_titles[webtoon_titles['day'] == current_day]  # 현재 요일의 웹툰만 필터링
+    total_titles = len(filtered_titles)
+
+    all_comments = []
+
+    for idx, row in enumerate(filtered_titles.iterrows()):
+        _, row = row
+        current_title_num = idx + 1  # 0-based index to 1-based index
+        contents = get_webtoon_title_contents(row['title'], row['webtoon_link'])
+        comments = run_multi_process_get_comments(contents[['content_link', 'title', 'content_name']].values.tolist(), current_title_num, total_titles)
+        all_comments.extend(comments)
+        
+    load_to_s3(webtoon_titles,'title')
+    
+    load_to_s3(webtoon_contents, 'contents')
+    load_to_s3(webtoon_comments, 'omments')
+    time.sleep(3)
+    
+    load_to_s3(f'{SCRIPY_START_TIME}_log.txt', 'webtoon_log', data_type='log')
+    
+            # 로그 파일 초기화 (옵션)
+    with open(f'{SCRIPY_START_TIME}_log.txt', 'w') as f:
+        f.truncate()
+    # pd.DataFrame(all_comments, columns=['title', 'content_name', 'nickname', 'id', 'comment_date', 'comment', 'scrapy_time']).to_csv('result_webtoon.csv', index=False)
+if __name__ == '__main__':
+    main()
+
