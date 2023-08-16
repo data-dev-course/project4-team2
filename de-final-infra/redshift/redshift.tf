71c243c42ab95a4caf40fdd34e3622d9f9970ef2
joyunji <dbtmxlsk2007@naver.com>
2023-08-16 05:22:03 +0000

    add redshift

---
+++ de-final-infra/redshift/redshift.tf
@@ -0,0 +1,22 @@
+data "aws_secretsmanager_secret" "redshift" {
+ name = "dev/de-3-2/redshift"
+}
+
+data "aws_secretsmanager_secret_version" "secret_credentials" {
+ secret_id = data.aws_secretsmanager_secret.redshift.id
+}
+
+resource "aws_redshiftserverless_namespace" "redshift_ns" {
+  namespace_name = "de-3-2"
+  admin_username = jsondecode(data.aws_secretsmanager_secret_version.secret_credentials.secret_string)["de-3-2_redshift_username"]
+  admin_user_password = jsondecode(data.aws_secretsmanager_secret_version.secret_credentials.secret_string)["de-3-2_redshift_password"]
+
+}
+
+resource "aws_redshiftserverless_workgroup" "redshift_wg" {
+  namespace_name = aws_redshiftserverless_namespace.redshift_ns.namespace_name
+  workgroup_name = "de-3-2-redshift"
+  base_capacity = 128
+  security_group_ids = [data.terraform_remote_state.vpc.outputs.aws_security_group_default_id]
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
+}
