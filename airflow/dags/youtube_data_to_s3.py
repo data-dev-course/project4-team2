312fa2db69ecf3b29c77c4db4b6f374709998693
yunji jo <dbtmxlsk2007@naver.com>
2023-08-19 05:09:37 +0900

    test

--- airflow/dags/youtube_data_to_s3.py
+++ airflow/dags/youtube_data_to_s3.py
@@ -12,19 +12,21 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 def upload_to_s3(data):
     print("========================")
     print(data[0])
-    print(data[1].head(2))
     src_date = data[0]
     s3_hook = S3Hook()
     s3_bucket = 'de-3-2'
     s3_folder = 'raw_data/youtube/'
-    s3_key = f'{s3_folder}/{src_date}/{src_date}.csv'
+    # s3_key = f'{s3_folder}/{src_date}/{src_date}.csv'
+    s3_key = f'{s3_folder}/test.txt'
+    
     
     s3_hook.load_string(
-        string_data= data[1],
+        string_data= 'hello',
         key=s3_key,
         bucket_name=s3_bucket,
         replace=True
     )
+    print(f"youtube data uploaded to {s3_bucket}/{s3_key}")
 
 api_service_name = "youtube"
 api_version = "v3"
