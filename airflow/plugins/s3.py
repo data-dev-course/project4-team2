d4cafc86096c223c355fe6a8fdee08594dcd4d4b
yunji jo <dbtmxlsk2007@naver.com>
2023-08-19 05:14:39 +0900

    test

--- airflow/plugins/s3.py
+++ airflow/plugins/s3.py
@@ -4,10 +4,10 @@ from airflow.models import Variable
 def upload_to_s3(**kwargs):
     print(kwargs['name'])
     print(kwargs['name'])
-    # s3_hook = S3Hook(aws_conn_id=s3_conn_id)
-    # s3_bucket = Variable.get("s3_bucket_name")
-    # s3_folder = 'raw_data/youtube/'
-    # s3_key = f'{s3_folder}/{dataframe[0]/dataframe[0].csv}'
+    s3_hook = S3Hook(aws_conn_id=s3_conn_id)
+    s3_bucket = 'de-3-2'
+    s3_folder = 'raw_data/youtube/'
+    s3_key = f'{s3_folder}/{dataframe[0]/dataframe[0].csv}'
     
     
     # s3_hook.load_string(
