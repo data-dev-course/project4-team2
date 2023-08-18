651babea4257e878d2ccd175fcfaac6774d928fc
yunji jo <dbtmxlsk2007@naver.com>
2023-08-18 15:09:29 +0900

    add publicly_accessiblein redshift_wg

--- de-final-infra/redshift/redshift.tf
+++ de-final-infra/redshift/redshift.tf
@@ -17,6 +17,7 @@ resource "aws_redshiftserverless_workgroup" "redshift_wg" {
   namespace_name = aws_redshiftserverless_namespace.redshift_ns.namespace_name
   workgroup_name = "de-3-2-redshift"
   base_capacity = 128
+  publicly_accessible = true
   security_group_ids = [data.terraform_remote_state.vpc.outputs.aws_security_group_default_id]
   subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
 }
