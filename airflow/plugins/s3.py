b8279bdf5f54a3151e694f3272256d3badc34fc7
yunji jo <dbtmxlsk2007@naver.com>
2023-08-18 23:20:58 +0900

    add s3.py

---
+++ airflow/plugins/s3.py
@@ -0,0 +1,18 @@
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.models import Variable
+
+def upload_to_s3(**kwargs):
+    print(kwargs['name'])
+    # s3_conn_id = Variable.get("s3_conn_id")
+    # s3_hook = S3Hook(aws_conn_id=s3_conn_id)
+    # s3_bucket = Variable.get("s3_bucket_name")
+    # s3_folder = 'raw_data/youtube/'
+    # s3_key = f'{s3_folder}/{dataframe[0]/dataframe[0].csv}'
+    
+    
+    # s3_hook.load_string(
+    #     string_data=dataframe,
+    #     key=s3_key,
+    #     bucket_name=s3_bucket,
+    #     replace=True
+    # )
