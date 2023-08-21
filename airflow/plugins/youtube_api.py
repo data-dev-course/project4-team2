401e7f2ee712b3769770b2c188b24f0e8dc5d49a
yunji jo <dbtmxlsk2007@naver.com>
2023-08-22 01:22:05 +0900

    add category_list

--- airflow/plugins/youtube_api.py
+++ airflow/plugins/youtube_api.py
@@ -11,6 +11,7 @@ api_service_name = "youtube"
 api_version = "v3"
 DEVELOPER_KEY = Variable.get("youtube_api_key")
 
+category_list = {'1': 'Film & Animation','2': 'Autos & Vehicles','10': 'Music','15': 'Pets & Animals','17': 'Sports','18': 'Short Movies','19': 'Travel & Events','20': 'Gaming','21': 'Videoblogging','22': 'People & Blogs','23': 'Comedy','24': 'Entertainment','25': 'News & Politics','26': 'Howto & Style','27': 'Education','28': 'Science & Technology','30': 'Movies','31': 'Anime/Animation','32': 'Action/Adventure','33': 'Classics','34': 'Comedy','35': 'Documentary','36': 'Drama','37': 'Family','38': 'Foreign','39': 'Horror','40': 'Sci-Fi/Fantasy','41': 'Thriller','42': 'Shorts','43': 'Shows','44': 'Trailers'}
 
 def youtube_search(category_id, publishedAfter, publishedBefore):
     
@@ -99,9 +100,6 @@ def get_data():
     comments = []
     start_date = datetime.utcnow().replace(hour=0, minute=0, second=0) - timedelta(weeks=12) 
     end_date = start_date + timedelta(days=1)  
-
-    category_list = {'1': 'Film & Animation','2': 'Autos & Vehicles','10': 'Music','15': 'Pets & Animals','17': 'Sports','18': 'Short Movies','19': 'Travel & Events','20': 'Gaming','21': 'Videoblogging','22': 'People & Blogs','23': 'Comedy','24': 'Entertainment','25': 'News & Politics','26': 'Howto & Style','27': 'Education','28': 'Science & Technology','30': 'Movies','31': 'Anime/Animation','32': 'Action/Adventure','33': 'Classics','34': 'Comedy','35': 'Documentary','36': 'Drama','37': 'Family','38': 'Foreign','39': 'Horror','40': 'Sci-Fi/Fantasy','41': 'Thriller','42': 'Shorts','43': 'Shows','44': 'Trailers'}
-
 
     for category_id in category_list:
         for item in youtube_search(category_id, convert(start_date),convert(end_date)):
