3f3374f649f34a7d83164bf1605b1e48709a75c2
yunji jo <dbtmxlsk2007@naver.com>
2023-08-22 01:19:46 +0900

    update

--- airflow/dags/youtube_copy_to_redshift.py
+++ airflow/dags/youtube_copy_to_redshift.py
@@ -1,38 +1,42 @@
 from airflow import DAG
 from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
 from airflow.providers.amazon.aws.operators.redshift_sql import RedshiftSQLOperator
+from datetime import datetime, timedelta
 
-
+scr_date = datetime.now().strftime('%Y-%m-%d')
 default_args = {
-    
+    'retries': 1,
+    'retry_delay': timedelta(minutes=3)
 }
 with DAG(
     dag_id="youtube_copy_to_redshift",
     default_args = default_args ,
+    start_date=datetime(2022, 8, 1),
     catchup=False,
-    schedule_interval=None
+    schedule_interval="10 0 * * *"
 )as dag:
     
 
     check_table_created_task = RedshiftSQLOperator(
         task_id='check_table_created',
-        redshift_conn_id = 'redshift_conn_id',
+        redshift_conn_id = 'redshift_dev_db',
         autocommit = True,
         sql = 
         """
         CREATE TABLE IF NOT EXISTS raw_data.tb_youtube (
             scr_date TIMESTAMP,
             video_id VARCHAR(128),
-            channel_id TIMESTAMP,
-            title TIMESTAMP,
+            channel_id VARCHAR(128),
+            title VARCHAR(1028),
+            category VARCHAR(10),
             video_published_at TIMESTAMP,
-            video_link VARCHAR(1024) 
-            comment_id TIMESTAMP,
-            author VARCHAR(128),
-            comment TEXT,
+            video_link TEXT,
+            comment_id VARCHAR(128),
+            author VARCHAR(256),
+            comment VARCHAR(30000),
             comment_publishd_at TIMESTAMP,
             comment_date TIMESTAMP,
-            tag VARCHAR(15)
+            "tag" VARCHAR(20)
         );
         """ 
     )
@@ -42,8 +46,8 @@ with DAG(
         schema='raw_data',
         table='tb_youtube',
         s3_bucket='de-3-2',
-        s3_key='raw_data/youtube/',
-        redshift_conn_id = 'redshift_conn_id',
+        s3_key=f'raw_data/youtube/{scr_date}/{scr_date}.csv',
+        redshift_conn_id = 'redshift_dev_db',
         copy_options=[
             "CSV IGNOREHEADER 1",
             "DATEFORMAT AS 'auto'",
@@ -51,7 +55,7 @@ with DAG(
         ],
         method='APPEND'
     )
-         
+
     check_table_created_task >> copy_to_redshift_task 
         
     
