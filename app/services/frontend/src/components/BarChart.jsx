575f5105a464ee8eaa6703004ecb573d4ce3436e
Dahye Kang <Dahye.Kang@ibm.com>
2023-08-30 01:59:14 +0900

    update chart

--- app/services/frontend/src/components/BarChart.jsx
+++ app/services/frontend/src/components/BarChart.jsx
@@ -1,17 +1,28 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import "../App.css";
 
 function BarChart(props) {
+    const [valueType, setValueType] = useState("all")
     return (
         <div className="bar-chart w-full min-w-[320px] px-1">
-            <div className="chart-header w-full flex flex-row gap-4 items-baseline">
+            <div className="chart-header w-full flex flex-row gap-4 items-baseline justify-start">
                 <div className="chart-title text-lg font-bold text-left">{props.title}</div>
                 <div className="chart-time text-md text-[#c2c1c1]">
                     2023-08-23 14:00
                 </div>
-                <div className="chart-select"></div>
             </div>
-            <div className="chart min-w-[280px] p-5 min-h-fit grid grid-cols-10 gap-1">
+            <div className={`w-full flex justify-end pt-2 ${props.selectHidden}`}>
+                <select className="chart-select rounded-[32px] px-2.5 py-0.5 text-white bg-[#3757CC] text-md" 
+                    name="valuetype" value={valueType} 
+                    onChange={(e) => setValueType(e.target.value)}>
+                        <option value="all"> 전체 ↓</option>
+                        <option value="youtube"> 유튜브 </option>
+                        <option value="news"> 뉴스 </option>
+                        <option value="news"> 웹툰 </option>
+                </select>
+            </div>
+            <div className="chart min-w-[300px] px-2.5 py-5 min-h-fit grid grid-cols-10 gap-1">
                 {
                     props.data.map((elem, i) => (
                         <span key={i} 
